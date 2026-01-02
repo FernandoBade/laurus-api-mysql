f9b5f69a48aaacb2231f9ff7007d21d1cf26c71e
Fernando Bade <89167737+FernandoBade@users.noreply.github.com>
2026-01-02 11:21:17 -0300

    ajuste de conexões

--- src/db/client.ts
+++ src/db/client.ts
@@ -10,10 +10,10 @@ dotenv.config();
  * Manages connection pooling for performance and scalability.
  */
 const pool = mysql.createPool({
-    host: process.env.DB_HOST ?? process.env.DB_URL,
+    host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE ?? process.env.DB_NAME,
+    database: process.env.DB_DATABASE,
     waitForConnections: true,
     connectionLimit: 10,
     queueLimit: 0,
